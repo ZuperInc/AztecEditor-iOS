8cfc97c3daa8c81243fddffb3d2078c62d320e34
Sabarinathan J <sabari@zuper.co>
2022-11-02 17:31:25 +0530

    Add SPM support for Aztec

---
+++ Sources/Zuper_AztecEditor-iOS/Zuper_AztecEditor_iOS.swift
@@ -0,0 +1,6 @@
+public struct Zuper_AztecEditor_iOS {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/Zuper_AztecEditor-iOSTests/Zuper_AztecEditor_iOSTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Zuper_AztecEditor_iOS
+
+final class Zuper_AztecEditor_iOSTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Zuper_AztecEditor_iOS().text, "Hello, World!")
+    }
+}
